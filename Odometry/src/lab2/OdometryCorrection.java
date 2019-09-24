85ef26304b857fa9e750bbd47a70e28c283aa084
tangj0 <jtang186@gmail.com>
2019-09-24 14:06:36 -0400

    final numbers for lab2 demo

--- Odometry/src/lab2/OdometryCorrection.java
+++ Odometry/src/lab2/OdometryCorrection.java
@@ -6,10 +6,8 @@ import lejos.hardware.Sound;
 
 public class OdometryCorrection implements Runnable {
   private static final long CORRECTION_PERIOD = 10;
-  private static final float SAMPLING_PERIOD = 0.001f; // 1ms time between each sample taken
-  //public static final int blackThreshold = 90; // 90 Tested value of color blackThreshold
+  public static final int blackThreshold = 88; // 90 Tested value of color blackThreshold
   private float colorData[]; // Array to store sensor data
-  private float lastColor; // Record last color to calibrate intensity
   private SampleProvider sampleProvider;
   
   private boolean onLine; // Boolean to prevent false positives
@@ -18,7 +16,7 @@ public class OdometryCorrection implements Runnable {
   private double xOffset, yOffset; //Offsets from origin
   
   //For calculating x and y Offset by taking (temp1 + temp2) / 2
-  double tempXOffset1, tempXOffset2, tempXOffset3, tempYOffset1, tempYOffset2; 
+  double tempXOffset1, tempXOffset2, tempYOffset1, tempYOffset2; 
 
   public OdometryCorrection() {
     sampleProvider = colorSensor.getMode("Red"); // Set sensor mode
@@ -31,11 +29,6 @@ public class OdometryCorrection implements Runnable {
   public void run() {
     long correctionStart, correctionEnd;
     
-    // Set initial lastColor
-    sampleProvider.fetchSample(colorData, 0);
-    colorData[0] *= 1000; // Scale up color intensity
-    lastColor = colorData[0]; 
-    
     while (true) {
       correctionStart = System.currentTimeMillis();
       sampleProvider.fetchSample(colorData, 0);
@@ -44,8 +37,7 @@ public class OdometryCorrection implements Runnable {
       xyt[1] = odometer.getXYT()[1]; // Set y 
       
       // Calculate and update odometer with new accurate positions
-      // If the slope of the intensity-time graph suddenly drops = black line
-      if ((lastColor -colorData[0])/SAMPLING_PERIOD < 0 && !onLine) {
+      if (colorData[0] < blackThreshold) {
         count++;
         Sound.playNote(Sound.FLUTE, 440, 250); 
         onLine = true;
@@ -73,12 +65,9 @@ public class OdometryCorrection implements Runnable {
           xyt[0] = (count-3)*TILE_SIZE;
         }
         else if (count > 9 && count <= 12) {
-          if (count == 10) {
-            tempXOffset2 = odometer.getXYT()[0]; //getting left side offset
-          }
           if (count == 12) {
             tempYOffset2 = odometer.getXYT()[1]; //getting bottom of offset
-            tempXOffset3 = odometer.getXYT()[0]; 
+            tempXOffset2 = odometer.getXYT()[0] - TILE_SIZE; 
           }  
           xyt[0] = 3*TILE_SIZE - ((count -10)*TILE_SIZE);
         }
@@ -86,18 +75,15 @@ public class OdometryCorrection implements Runnable {
           break;
         }
         odometer.setXY(xyt[0], xyt[1]); 
-        lastColor = colorData[0]; //update lastColor
       }
       onLine = false; // Update boolean
 
       LCD.drawString("Count: " + count, 0, 3);
-      LCD.drawString("lastColor " + lastColor, 0, 5);
-      LCD.drawString("Color Data" + colorData[0], 0, 6);
       
-//      LCD.drawString("x1: " + tempXOffset1, 0, 4);
-//      LCD.drawString("x2: " + tempXOffset3, 0, 5);
-//      LCD.drawString("y1: " + tempYOffset1 , 0, 6);
-//      LCD.drawString("y2: " + tempYOffset2, 0, 7);
+      LCD.drawString("x1: " + tempXOffset1, 0, 4);
+      LCD.drawString("x2: " + tempXOffset2, 0, 5);
+      LCD.drawString("y1: " + tempYOffset1 , 0, 6);
+      LCD.drawString("y2: " + tempYOffset2, 0, 7);
       
       if (count == 12) {
         xOffset = (tempXOffset1 + tempXOffset2)/2;
--- Odometry/src/lab2/Resources.java
+++ Odometry/src/lab2/Resources.java
@@ -23,13 +23,13 @@ public class Resources {
   /**
    * The robot width in centimeters.
    */
-  public static final double TRACK = 12.43; //12.4, 10.7, 12.3
+  public static final double TRACK = 12.25; //12.43, 10.7, 12.3
   
   /**
    * The speed at which the robot moves forward in degrees per second.
    */
   
- public static final float FORWARD_SPEED = 150; 
+ public static final float FORWARD_SPEED = 170; 
   
   
   /**
